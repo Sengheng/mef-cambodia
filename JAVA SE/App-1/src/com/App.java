910c7aa260902bfc9d87f410ede5ca5decef479e
richardsforu <praveen.somireddy@gmail.com>
2017-12-04 12:29:39 +0530

    Java PPTS added

---
+++ JAVA SE/App-1/src/com/App.java
@@ -0,0 +1,73 @@
+package com;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	/*	int a=100,b=200;
+		
+		System.out.println("Hello World");
+		System.out.println("Hello World too");
+
+		System.out.println(a+b);
+		System.out.println("Sum is "+(a+b));
+		System.out.println(" Sum of "+a+" and "+b+" is "+(a+b));
+		*/
+				
+			/*byte b=127;
+			System.out.println("byte: "+b );
+			short s=32767;
+			System.out.println("Short is "+s);
+			int x=214748364; // 4
+			System.out.println("Int: "+x);
+			long l=9223372036854775807L ; // 8
+			System.out.println("Long: "+l);*/
+			
+			// Character SET: UNICODE and ASCII
+		
+		  // --> UNICODE - 2 bytes -> internationalization (i18n)
+			
+		/*for(int i=65;i<=90;i++) {
+			System.out.print((char)i+" ");
+		}
+		System.out.println("\n---------------------------------------");	
+		
+		for(int i=97;i<=122;i++) {
+			System.out.print((char)i+" ");
+		}*/
+		/*
+		float f=87.34f; // 4 bytes
+		double d=87.34; // 8 bytes 
+*/		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ JAVA SE/App-1/src/com/Employee.java
@@ -0,0 +1,47 @@
+package com;
+
+public class Employee {
+
+	/*
+	 * Notes:
+	 * ---------
+	 * -> you can not run a program which contains no main method.
+	 * 
+	 * What can be in class
+	 * 1. variables / properties
+	 * 2. methods
+	 * 3. constructors
+	 * 
+	 * variables
+	 * ------------
+	 * -> instance
+	 * -> static
+	 * -> const
+	 * -> final
+	 * -> volatile
+	 * -> transient
+	 * 
+	 * methods
+	 * --------
+	 * -> instance methods
+	 * -> static methods
+	 * -> abstract methods
+	 * -> final methods
+	 * -> native methods
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+	}
+	
+	
+}
